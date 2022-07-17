5a40297a8a48ba337bd3043840a0a0addd4bd795
reruy.dev <reruy.dev@gmail.com>
2022-07-17 13:14:15 +0200

    Initial Commit

---
+++ prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba
+//
+//  Created by Raúl Rivero Gomez on 17/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ prueba/pruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaApp.swift
+//  prueba
+//
+//  Created by Raúl Rivero Gomez on 17/7/22.
+//
+
+import SwiftUI
+
+@main
+struct pruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
